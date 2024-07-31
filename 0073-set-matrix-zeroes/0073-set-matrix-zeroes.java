a7f17a1b7860e0d414fea40203e0b921a0f4b83d
Arun Kumar <90148777+ArunKumar-07@users.noreply.github.com>
2024-07-31 14:53:50 +0530

    Time: 2 ms (19.02%), Space: 45.6 MB (24.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,38 +1,33 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-       Stack<int[]> st = new Stack<>();
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j] == 0) st.push(new int[]{i,j});
+      int n = matrix.length;
+        int m = matrix[0].length;
+        boolean row = false;
+        boolean col = false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    if(i==0) row = true;
+                    if(j==0) col= true;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
             }
         }
-        while(!st.isEmpty()){
-            int[] curr = st.pop();
-        //logic
-            findUp(curr[0],curr[1],matrix);
-             findDown(curr[0],curr[1],matrix);
-             findRight(curr[0],curr[1],matrix);
-             findLeft(curr[0],curr[1],matrix);
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0] ==0 || matrix[0][j] == 0) matrix[i][j]=0;
+            }
         }
-    }
-    void findUp(int i,int j, int[][] matrix){
-        if(i < 0) return;
-        matrix[i][j]=0;
-        findUp(i-1,j,matrix);
-    }
-     void findDown(int i,int j, int[][] matrix){
-        if( matrix[0].length <= j ) return;
-        matrix[i][j]=0;
-        findDown(i,j+1,matrix);
-    }
-     void findRight(int i,int j, int[][] matrix){
-        if(  matrix.length  <= i) return;
-        matrix[i][j]=0;
-        findRight(i+1,j,matrix);
-    }
-     void findLeft(int i,int j, int[][] matrix){
-        if(j < 0) return;
-        matrix[i][j]=0;
-        findLeft(i,j-1,matrix);
+       if(row){
+           for(int j=0;j<m;j++) {
+               matrix[0][j]=0;
+           }
+       }
+        if(col){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
     }
 }
