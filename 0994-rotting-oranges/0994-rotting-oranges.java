59cb3804aa7df3dd42a37b8c5592482b7bf40165
Arun Kumar <90148777+ArunKumar-07@users.noreply.github.com>
2024-08-20 09:57:49 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (74.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,38 +1,36 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-       Queue<int[]> qu = new LinkedList<>();
+        Queue<int[]> qu = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
         int fresh = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 2 ){
-                    qu.offer(new int[]{i,j});
-                }else if(grid[i][j] == 1){
-                    fresh++;    
+                if(grid[i][j] ==2){
+                    qu.add(new int[]{i,j});
                 }
+                if(grid[i][j] ==1) fresh++;
             }
         }
-        if(fresh == 0) return 0;
-        int countTime = 0;
-        int[] dx = {0,0,1,-1};
+        if(fresh ==0 ) return 0;
+        int minute =0;
+        int[] dx = {0,0,-1,1};
         int[] dy = {1,-1,0,0};
         while(!qu.isEmpty()){
-            int size = qu.size();
-            for(int i=0;i<size;i++){
-                int[] value = qu.poll();
+            int len = qu.size();
+            for(int i=0;i<len;i++){
+                int[] curr = qu.poll();
                 for(int j=0;j<4;j++){
-                    int x = value[0] + dx[j];
-                    int y = value[1] + dy[j];
-                    if(x < 0 || y < 0 || x>=n || y >=m || grid[x][y] ==2 || grid[x][y] ==0 ) continue;
-                    if(grid[x][y] ==1 ) fresh--;
-                    grid[x][y] =2 ;
-                    qu.offer(new int[]{x,y});
+                    int x = curr[0] + dx[j];
+                    int y = curr[1] + dy[j];
+    if(x<0 ||y<0 || x>=n||y>=m|| grid[x][y]==0 || grid[x][y]==2) continue;
+                    if(grid[x][y]==1) fresh--;
+                    grid[x][y] =2;
+                    qu.add(new int[]{x,y});
                 }
             }
-            if(qu.size() != 0) countTime++;
-        }
-        return fresh == 0 ? countTime : -1;
-   
+           if(qu.size()!=0) minute++;
+        }        
+    return fresh ==0 ? minute : -1; 
     }
 }
