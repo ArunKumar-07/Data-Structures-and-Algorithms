bd2571b68e67930513e68631f74a1cbc3e752675
Arun Kumar <90148777+ArunKumar-07@users.noreply.github.com>
2024-09-23 20:01:03 +0530

    Time: 1 ms (94.42%), Space: 44.8 MB (12.38%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,18 +1,18 @@
 class Solution {
-public List<List<Integer>> permute(int[] nums) {
-   List<List<Integer>> outer = new ArrayList<>();
-    find(outer,new ArrayList<>(),nums);
-    return outer;
-}
-    static  void find(List<List<Integer>> outer , List<Integer> list , int[] nums){
-        if(list.size() == nums.length) {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+        permutation(nums,outer,new ArrayList<>());
+        return outer;
+    }
+    static void permutation(int[] nums,List<List<Integer>> outer,ArrayList<Integer> list){
+        if(list.size()== nums.length){
             outer.add(new ArrayList<>(list));
             return;
-        }else {
-            for(int i=0;i< nums.length;i++){
-                if(list.contains(nums[i])) continue;
+        }else{
+            for(int i=0;i<nums.length;i++){
+               if(list.contains(nums[i])) continue;
                 list.add(nums[i]);
-                find(outer,list,nums);
+                permutation(nums,outer,list);
                 list.remove(list.size()-1);
             }
         }
