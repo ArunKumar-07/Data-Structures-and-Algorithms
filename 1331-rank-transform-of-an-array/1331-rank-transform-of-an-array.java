7f4f72039a7b1a55149d29237f7103d51cc931be
Arun Kumar <90148777+ArunKumar-07@users.noreply.github.com>
2024-10-02 19:56:52 +0530

    Time: 23 ms (93.80%), Space: 62.4 MB (29.84%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+        int[] dup =  new int[n];
+       for(int i=0;i<n;i++){
+           dup[i] = arr[i];
+       }
+        Arrays.sort(dup);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int pp = 1;
+        for(int i=0;i<  dup.length;i++){
+            if(map.containsKey(dup[i])) continue;
+           map.put(dup[i],pp++);
+        }
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            int val = map.get(arr[i]);
+            ans[i] = val;
+        }
+        return ans;
+    }
+}
