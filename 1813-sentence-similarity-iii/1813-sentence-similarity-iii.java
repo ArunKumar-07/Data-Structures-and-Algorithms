f592f465b22dfc587f399dec58a7a29a92fa9556
Arun Kumar <90148777+ArunKumar-07@users.noreply.github.com>
2024-10-07 00:54:33 +0530

    Time: 3 ms (15.48%), Space: 41.8 MB (39.29%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean areSentencesSimilar(String s1, String s2) {
+        Deque<String> deque1 = new ArrayDeque<>(Arrays.asList(s1.split(" ")));
+        Deque<String> deque2 = new ArrayDeque<>(Arrays.asList(s2.split(" ")));
+        while (
+            !deque1.isEmpty() &&
+            !deque2.isEmpty() &&
+            deque1.peek().equals(deque2.peek())
+        ) {
+            deque1.poll();
+            deque2.poll();
+        }
+        while (
+            !deque1.isEmpty() &&
+            !deque2.isEmpty() &&
+            deque1.peekLast().equals(deque2.peekLast())
+        ) {
+            deque1.pollLast();
+            deque2.pollLast();
+        }
+        return deque1.isEmpty() || deque2.isEmpty();
+    }
+}
