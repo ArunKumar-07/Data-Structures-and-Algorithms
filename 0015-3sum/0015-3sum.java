01245b63f1a5e8c7414e134c6e6b6ee67300c49c
Arun Kumar <90148777+ArunKumar-07@users.noreply.github.com>
2024-10-07 15:15:56 +0530

    Time: 850 ms (12.45%), Space: 52.2 MB (32.02%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-         List<List<Integer>>outer = new ArrayList<>();
-         Set<List<Integer>> set=new HashSet<>();
-            
-        for( int i=0;i<nums.length;i++){
-            int j = i+1;
-            int k=nums.length-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    set.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    j++;
-                    k--;
-                }
-               else if(sum<0){
-                    j++;
-                }else{
-                    k--;
-                }
-            }
-        }    
-        outer.addAll(set);
+         List<List<Integer>> outer = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+         for(int i=0;i<nums.length;i++){
+             int j=i+1;
+             int k =nums.length-1;
+             while(j < k){
+                 int value = nums[i] + nums[j] + nums[k];
+                 if(value ==0){
+                    set.add(Arrays.asList(nums[i],nums[j],nums[k])); 
+                     j++;
+                     k--;
+                 }
+                  else if(value > 0 ){
+                     k--;
+                 }else{
+                      j++;
+                  }
+             }
+         }
+          outer.addAll(set);
         return outer;
-     }
+    }
 }
